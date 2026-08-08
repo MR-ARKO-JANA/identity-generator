0eea7e06dd39af176bf080875ffd17080757e197
Arko Jana <arkojana45@gmail.com>
2026-08-08 13:07:04 +0530

    build(dist): update production compiled static output in docs directory

--- docs/_next/static/rf89yx6ttlaPcFSt0c2D_/_buildManifest.js
+++ docs/_next/static/rf89yx6ttlaPcFSt0c2D_/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/identity-generator//_next/:path+",
+        "destination": "/identity-generator/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
